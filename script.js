76967c36e4a2a6e3d17aeff2af1f2e5f701a1522
Oyinlola Rofiat <rofiatoyinlola07@gmail.com>
2025-06-17 17:17:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let cartCount = 0;
 
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    cartCount++;
+    document.getElementById("cart-count").textContent = cartCount;
+  });
+});
